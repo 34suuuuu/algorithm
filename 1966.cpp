a9433e9a20e2c8e191f32e184026f4d33b97318a
suhyunpark <psh7492@gmail.com>
2021-07-05 20:49:39 +0900

    Update 1966.프린터 큐

---
+++ 1966.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+    int count=0;
+    int num, index, imp;
+    int test; cin>>test;
+    
+    for(int i =0; i<test; ++i){
+        count =0;
+        cin>>num>>index;
+        queue<pair<int, int>> q;
+        priority_queue<int>pq;
+        
+        for(int j =0; j<num; ++j){
+            cin>>imp;
+            q.push({j,imp});
+            pq.push(imp);
+        }
+        while(!q.empty()){
+            int first = q.front().first;
+            int second = q.front().second;
+            
+            q.pop();
+            if(pq.top() == second){
+                pq.pop();
+                ++count;
+                if(first == index){
+                    cout<<count<<'\n';
+                    break;
+                }
+            }
+            else q.push({first, second});
+        }
+    }
+}
