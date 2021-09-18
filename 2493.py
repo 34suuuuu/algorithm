8626195b0f8e5c60ead8fbc939a5d79fa3398369
suhyunpark <psh7492@gmail.com>
2021-09-18 10:45:02 +0900

    Update 2493.탑

--- 2493.py
+++ 2493.py
@@ -1,9 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-temp = list(map(int, input().split()))
-
+N = int(input())
+topList = list(map(int, input().split()))
+ans = []
 stack = []
-for i in temp:
-    stack.append(i)
+
+for i in range(N):
+
+    while stack:
+        if stack[-1][1] > topList[i]:
+            ans.append(stack[-1][0] + 1)
+            break
+        else:
+            stack.pop()
+
+    if not stack:
+        ans.append(0)
+
+    stack.append([i, topList[i]])
+
+for _ in ans:
+    print(_, end=" ")
