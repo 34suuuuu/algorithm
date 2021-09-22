34918b01698f50a19803fe13975c0f7b73347349
suhyunpark <psh7492@gmail.com>
2021-09-22 14:21:45 +0900

    Update 1655:시간초과

---
+++ 1655.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+# 시간초과
+# N = int(input())
+# data = []
+# for i in range(N):
+#     num = int(input())
+#     data.append(num)
+#     data.sort()
+#     half = len(data) // 2
+#     if len(data) % 2 == 0:
+#         print(data[half-1])
+#     else:
+#         print(data[half])
