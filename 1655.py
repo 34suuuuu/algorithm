3579df27ff7b8178c5a673779b7830453ee7a434
suhyunpark <psh7492@gmail.com>
2021-09-24 19:21:32 +0900

    Update 1655.가운데를말해요

--- 1655.py
+++ 1655.py
@@ -17,7 +17,23 @@ input = sys.stdin.readline
 
 
 N = int(input())
-heap = []]
+left = []
+right = []
+result = []
+
 for i in range(N):
     num = int(input())
-    heap.heappush(heap, num)
+
+    if len(left) == len(right):  # 짝수일 때
+        heapq.heappush(left, (-num, num))
+    else:   # 홀수일 떄
+        heapq.heappush(right, (num, num))
+
+    if right and left[0][1] > right[0][0]:
+        min = heapq.heappop(right)[0]
+        max = heapq.heappop(left)[1]
+        heapq.heappush(left, (-min, min))
+        heapq.heappush(right, (max, max))
+    result.append(left[0][1])
+for _ in result:
+    print(_)
