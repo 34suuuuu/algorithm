6c585e9e9d4f213d09d5bb3f75c3186d75ca161f
suhyunpark <psh7492@gmail.com>
2021-10-01 16:12:57 +0900

    부족한 금액 계산하기

---
+++ programmers/WeeklyChallenge01.py
@@ -0,0 +1,12 @@
+def solution(price, money, count):
+    answer = -1
+    sum = 0
+
+    for i in range(count+1):
+        sum += (price * i)
+    if sum > money:
+        answer = (sum - money)
+    else:
+        answer = 0
+
+    return answer
