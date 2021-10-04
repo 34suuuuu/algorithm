f7d83e505e9eb766dbd31d0dc35ba1a0ce98f309
suhyunpark <psh7492@gmail.com>
2021-10-04 18:12:38 +0900

    직업군추천하기

---
+++ programmers/WeeklyChallenge03.py
@@ -0,0 +1,44 @@
+def solution(table, languages, preference):
+    answer = ''
+    score = []
+
+    new_list = []
+    for i in range(len(table[0].split())-1):
+        new_list.append(table[i].split())
+
+    length = len(new_list)
+    for i in range(length):
+        sum = 0
+        for j in range(len(languages)):
+            if languages[j] in new_list[i]:
+                sum += ((6 - new_list[i].index(languages[j])) * preference[j])
+
+        score.append(sum)
+
+    index = score.index(max(score))
+    answer = new_list[index][0]
+
+    m = max(score)
+    max_index = [i for i, v in enumerate(score) if v == m]
+    max_list = [new_list[i][0] for i in max_index]
+    max_list.sort()
+    answer = max_list[0]
+    return answer
+
+
+table = ["SI JAVA JAVASCRIPT SQL PYTHON C#",
+         "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
+         "HARDWARE C C++ PYTHON JAVA JAVASCRIPT",
+         "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP",
+         "GAME C++ C# JAVASCRIPT C JAVA"]
+languages = ["PYTHON", "C++", "SQL"]
+preference = [7, 5, 5]
+
+print(solution(table, languages, preference))
+
+table = ["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
+         "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"]
+languages = ["JAVA", "JAVASCRIPT"]
+preference = [7, 5]
+
+print(solution(table, languages, preference))
