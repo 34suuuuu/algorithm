d354e3d62beee61dd0400f140b4551291691bb83
suhyunpark <psh7492@gmail.com>
2021-10-04 22:55:11 +0900

    직업군추천하기-dict이용

--- programmers/WeeklyChallenge03.py
+++ programmers/WeeklyChallenge03.py
@@ -1,29 +1,41 @@
+# def solution(table, languages, preference):
+#     answer = ''
+#     score = []
+
+#     new_list = []
+#     for i in range(len(table[0].split())-1):
+#         new_list.append(table[i].split())
+
+#     length = len(new_list)
+#     for i in range(length):
+#         sum = 0
+#         for j in range(len(languages)):
+#             if languages[j] in new_list[i]:
+#                 sum += ((6 - new_list[i].index(languages[j])) * preference[j])
+
+#         score.append(sum)
+
+#     index = score.index(max(score))
+#     answer = new_list[index][0]
+
+#     m = max(score)
+#     max_index = [i for i, v in enumerate(score) if v == m]
+#     max_list = [new_list[i][0] for i in max_index]
+#     max_list.sort()
+#     answer = max_list[0]
+#     return answer
+
 def solution(table, languages, preference):
-    answer = ''
-    score = []
+    score = {}
+    for t in table:
+        for lang, pref in zip(languages, preference):
+            if lang in t.split():
+                score[t.split()[0]] = (6 - t.split().index(lang)) * \
+                    pref + score.get(t.split()[0], 0)
 
-    new_list = []
-    for i in range(len(table[0].split())-1):
-        new_list.append(table[i].split())
-
-    length = len(new_list)
-    for i in range(length):
-        sum = 0
-        for j in range(len(languages)):
-            if languages[j] in new_list[i]:
-                sum += ((6 - new_list[i].index(languages[j])) * preference[j])
-
-        score.append(sum)
-
-    index = score.index(max(score))
-    answer = new_list[index][0]
-
-    m = max(score)
-    max_index = [i for i, v in enumerate(score) if v == m]
-    max_list = [new_list[i][0] for i in max_index]
-    max_list.sort()
-    answer = max_list[0]
-    return answer
+    sorted_score = sorted(
+        score.items(), key=lambda item: (-item[1], item[0]))
+    return sorted_score[0][0]
 
 
 table = ["SI JAVA JAVASCRIPT SQL PYTHON C#",
