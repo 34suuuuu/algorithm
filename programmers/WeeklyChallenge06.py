5fa987173b0fd9a6e10855599a8f9143a8aa43c3
suhyunpark <psh7492@gmail.com>
2021-10-06 21:01:34 +0900

    update weeklychallenge06

---
+++ programmers/WeeklyChallenge06.py
@@ -0,0 +1,15 @@
+def solution(weights, head2head):
+    answer = []
+    victory = []
+
+    w = len(weights)
+    for i in range(w):
+        index = []
+
+    return answer
+
+
+weights = [50, 82, 75, 120]
+head2head = ["NLWL", "WNLL", "LWNW", "WWLN"]
+
+solution(weights, head2head)
