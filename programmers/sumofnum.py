f7dbb02047ced3ab03c07b208b8d511c33c64683
suhyunpark <psh7492@gmail.com>
2021-10-15 17:41:17 +0900

    두개뽑아서더하기

---
+++ programmers/sumofnum.py
@@ -0,0 +1,9 @@
+from itertools import permutations
+from itertools import combinations
+
+arr = ['A', 'B', 'C']
+permutations_list = list(permutations(arr, 2))
+combinations_list = list(combinations(arr, 2))
+
+print(permutations_list)
+print(cobinations_list)
