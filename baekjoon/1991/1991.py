10cbf399786297d49b832e74b9c8f7004f930a4a
suhyunpark <psh7492@gmail.com>
2022-04-13 12:35:30 +0900

    ADD 1991 python solution

---
+++ baekjoon/1991/1991.py
@@ -0,0 +1,44 @@
+# 1991 트리 순회
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+tree = {}
+
+for _ in range(n):
+    root, left, right = input().strip().split()
+    tree[root] = [left, right]
+
+# 루트 -> 왼쪽 -> 오른쪽
+
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+# 왼쪽 -> 루트 -> 오른쪽
+
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+
+# 왼쪽 -> 오른쪽 -> 루트
+
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
